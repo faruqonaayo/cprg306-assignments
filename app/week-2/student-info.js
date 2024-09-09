3dbf226d900e3e3d757400b7bfa7ce867a9a8b12
Faruq <onaayofaruq24@gmail.com>
2024-09-09 10:45:55 -0600

    Week 2 Assignment First Commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <>
+      <h1>Faruq Ayomide Ogunkunle</h1>
+      <Link href="https://github.com/faruqonaayo?tab=repositories">
+        My Repository Link
+      </Link>
+    </>
+  );
+}
