49c75e4cf79ea8ccdbc1a8ebc7d89b662dc417a3
Faruq <onaayofaruq24@gmail.com>
2024-11-05 14:19:53 -0700

    Complete week 8 assignment

---
+++ app/week-9/_utils/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZTYU_0aXLq92nR0xawUn3HDk29Zo8y7o",
+  authDomain: "cprg306-7f074.firebaseapp.com",
+  projectId: "cprg306-7f074",
+  storageBucket: "cprg306-7f074.appspot.com",
+  messagingSenderId: "302594696128",
+  appId: "1:302594696128:web:a46477d0f2bb27f8cade25",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
